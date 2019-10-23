f5ec87dafa4ec48bbde055a7f50dd8bf016e0766
msergazin <msergazin@gmail.com>
2019-10-23 17:06:02 +0200

    recursive funcs

--- src/main/scala/manning/ch6_purely_func_state/RNG.scala
+++ src/main/scala/manning/ch6_purely_func_state/RNG.scala
@@ -3,4 +3,10 @@ package manning.ch6_purely_func_state
 trait RNG {
   def nextInt: (Int, RNG)
   def nonNegativeInt(rng: RNG): (Int, RNG)
+  def double(rng: RNG): (Double, RNG)
+  def intDouble(rng: RNG): ((Int, Double), RNG)
+  def doubleInt(rng: RNG): ((Double, Int), RNG)
+  def double3(rng: RNG): ((Double,Double,Double), RNG)
+  def ints(count: Int)(rng: RNG): (List[Int], RNG)
+  def intsTailRecursive(count: Int)(rng: RNG): (List[Int], RNG)
 }
--- src/main/scala/manning/ch6_purely_func_state/SimpleRNG.scala
+++ src/main/scala/manning/ch6_purely_func_state/SimpleRNG.scala
@@ -10,19 +10,61 @@ case class SimpleRNG(seed: Long) extends RNG {
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, r) = rng.nextInt
-    if (i < 0) (-(i + 1),r) else (i,r)
+    (if (i < 0) -(i + 1) else i, r)
   }
 
+  override def double(rng: RNG): (Double, RNG) = {
+    val (i,r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
 
-  /*type Rand[+A] = RNG => (A, RNG)
-  val int: Rand[Int] = _.nextInt
+  override def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i,d),r2)
+  }
 
-  def unit[A](a: A): Rand[A] =
-    rng => (a, rng)*/
+  override def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i,d),r) = intDouble(rng)
+    ((d,i),r)
+  }
+
+  override def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1,r2) = double(rng)
+    val (d2,r3) = double(r2)
+    val (d3,r4) = double(r3)
+    ((d1,d2,d3),r4)
+  }
+
+  override def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count <= 0)
+      (List(),rng)
+    else {
+      val (x,r2) = rng.nextInt
+      val (xs,r3) = ints(count - 1)(r2)
+      (x :: xs, r3)
+    }
+  }
+
+  override def intsTailRecursive(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(count: Int, xs: List[Int], r: RNG): (List[Int], RNG) = {
+      if (count <= 0)
+        (xs, rng)
+      else {
+        val (x,r2) = rng.nextInt
+        go(count - 1, x :: xs, r2)
+      }
+    }
+    go(count, List(), rng)
+  }
 }
 
 object SimpleRNG extends App {
   val rng = SimpleRNG(42)
+//  println("list: " + rng.ints(1)(rng))
+  println("list1: " + rng.ints(5)(rng))
+  println("list2: " + rng.intsTailRecursive(5)(rng))
+
   /*val (n1, rng2) = rng.nextInt
   println(n1)
   println(rng2)
@@ -31,12 +73,17 @@ object SimpleRNG extends App {
   println(n2)
   println(rng3)
 
+  val (n3, rng4) = rng3.nextInt
+  println(n3)
+  println(rng4)
+*/
   /*def randomPair(rng: RNG): (Int, Int) = {
     val (i1, _) = rng.nextInt
     val (i2, _) = rng.nextInt
     (i1, i2)
   }*/
 
+  /*
   println(randomPair(rng))
   def randomPair(rng: RNG): ((Int, Int), RNG) = {
     val (i1, rng2) = rng.nextInt
@@ -44,6 +91,14 @@ object SimpleRNG extends App {
     ((i1,i2), rng3)
   }*/
 
-  println(rng.nonNegativeInt(rng))
+//  println(rng.nonNegativeInt(rng))
+//  val (n1, rng2) = rng.nonNegativeInt(rng)
+//  println(n1)
+//  println(rng2)
+//
+//  val (n2, rng3) = rng2.nonNegativeInt(rng2)
+//  println(n2)
+//  println(rng3)
 
+//  val (n3, rng4) = rng3.double(rng3)
 }
