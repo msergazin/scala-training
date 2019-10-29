0cd1e94b19b0d0a950b01d997231b731efab8de5
msergazin <msergazin@gmail.com>
2019-10-29 13:25:09 +0100

    moving stuff around

--- src/main/scala/manning/ch6_purely_func_state/RNG.scala
+++ src/main/scala/manning/ch6_purely_func_state/RNG.scala
@@ -1,20 +1,20 @@
-package manning.ch6_purely_func_state
-trait RNG {
-//  type Rand[+A] = RNG => (A, RNG)
-  type Rand[A] = State[RNG,A]
-
-  type State[S, +A] = S => (A,S)
-
-  def nextInt: (Int, RNG)
-  def nonNegativeInt(rng: RNG): (Int, RNG)
-  def double(rng: RNG): (Double, RNG)
-  def _double(rng: RNG): Rand[Double]
-  def intDouble(rng: RNG): ((Int, Double), RNG)
-  def doubleInt(rng: RNG): ((Double, Int), RNG)
-  def double3(rng: RNG): ((Double,Double,Double), RNG)
-  def ints(count: Int)(rng: RNG): (List[Int], RNG)
-  def intsTailRecursive(count: Int)(rng: RNG): (List[Int], RNG)
-//  def map[A,B](s: Rand[A])(f: A => B): Rand[B]
-  def map[S,A,B](a: S => (A,S))(f: A => B): S => (B,S)
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C]
-}
+//package manning.ch6_purely_func_state
+//trait RNG {
+////  type Rand[+A] = RNG => (A, RNG)
+////  type Rand[A] = State[RNG,A]
+////
+////  type State[S, +A] = S => (A,S)
+////
+////  def nextInt: (Int, RNG)
+////  def nonNegativeInt(rng: RNG): (Int, RNG)
+////  def double(rng: RNG): (Double, RNG)
+////  def _double(rng: RNG): Rand[Double]
+////  def intDouble(rng: RNG): ((Int, Double), RNG)
+////  def doubleInt(rng: RNG): ((Double, Int), RNG)
+////  def double3(rng: RNG): ((Double,Double,Double), RNG)
+////  def ints(count: Int)(rng: RNG): (List[Int], RNG)
+////  def intsTailRecursive(count: Int)(rng: RNG): (List[Int], RNG)
+//////  def map[A,B](s: Rand[A])(f: A => B): Rand[B]
+////  def map[S,A,B](a: S => (A,S))(f: A => B): S => (B,S)
+////  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C]
+//}
