64fab55e37ce4d971ac55ae7eab36da925cf5b92
msergazin <msergazin@gmail.com>
2019-11-05 16:58:42 +0100

    chapter 3

--- src/main/scala/manning/ch3/List.scala
+++ src/main/scala/manning/ch3/List.scala
@@ -6,10 +6,46 @@ case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a,b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1,t1), Cons(h2,t2)) => Cons(h1+h2, addPairwise(t1,t2))
+  }
+  def zipWith[A,B,C](a: List[A], b: List[B])(f: (A,B) => C): List[C] = (a,b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1,t1), Cons(h2,t2)) => Cons(f(h1,h2), zipWith(t1,t2)(f))
+  }
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+//    foldRight(as, Nil:List[A])((h,t) => if (f(h)) Cons(h,t) else t) //works
+    flatMap(as)(a => if (f(a)) List(a) else Nil)
+  }
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] =
+    concat(map(l)(f))
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, Nil:List[A])(append)
+  def map[A,B](as: List[A])(f: A => B): List[B] = {
+    foldRight(as, Nil: List[B])((h,t) => Cons(f(h), t))
+  }
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil:List[String])((h,t) => Cons(h.toString,t))
+  def add1(list: List[Int]): List[Int] = {
+    foldRight(list, Nil:List[Int])((h,t) => Cons(h+1, t))
+  }
+  def append[A](l: List[A], r: List[A]): List[A] = foldRight(l, r)(Cons(_,_))
   def length[A](as: List[A]): Int = {
 //    foldRight(as, 0)((_,acc) => acc + 1)
     foldLeft(as, 0)((acc, h) => acc + 1)
   }
+
+  /*1->2->3->Nil
+  * 1->(2->(3->List())
+  * 1->(2->(3)
+  * 1->(2<-(3)
+  * 1<-2<-3
+  *
+  * */
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, List[A]())((acc,h) => Cons(h,acc))
   def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
     as match {
       case Nil => z
@@ -94,12 +130,15 @@ object List {
     val ex2: List[Int] = Cons(1, Nil)
     val ex3: List[String] = Cons("a", Cons("b", Nil))
     val list: List[Double] = Cons(1,Cons(2, Cons(3, Nil)))
-//    println(product(list))
+
+    //    println(product(list))
 //    println(drop(list, 2))
 //    println(dropWhile(list, ((x: Double) => x  != 2)))
 //    println(dropWhile(list)(_  != 2))
 //    println(init(list))
-
+//    println(doubleToString(list))
+//    println(map(list)(_ + 1))
+    println(filter(list)(_ % 2 == 0))
 
   }
 }
