1fdc90af36d42e09be9292ac26816359983a200b
msergazin <msergazin@gmail.com>
2019-11-14 17:34:28 +0100

    chapter 5

--- src/main/scala/manning/ch5/Stream.scala
+++ src/main/scala/manning/ch5/Stream.scala
@@ -20,26 +20,69 @@ sealed trait Stream[+A] {
     case Cons(h,t) => h() :: t().toList
     case _ => Nil
   }
-  def headOption: Option[A] = this match {
+  def headOption: Option[A] = foldRight(None: Option[A])((a,_) => Some(a))
+  /*this match {
     case Empty => None
     case Cons(h, t) => Some(h())
-  }
+  }*/
 
   /*take(n) for returning the first n elements of a Stream*/
-  def take(n: Int): Stream[Any] = this match {
+  def take(n: Int): Stream[A] = this match {
     case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
     case Cons(h, _) if n == 1 => cons(h(), empty)
     case _ => empty
   }
 
   /*drop(n) for skipping the first n elements*/
-  def drop(n: Int): Stream[A] = {
-    def go(s: Stream[A], count: Int): Stream[A] = s match {
-      case Cons(h,t) => if (count > 0) go(t(), count) else cons(h(), go(t(), count - 1))
-      case _ => Empty
-    }
-    go(this, n)
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
   }
+  /*returning all starting elements of a Stream that match the given predicate*/
+  def takeWhile_1(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h,t) =>
+      if (f(h)) cons(h,t)
+      else      empty)
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h,t) =>
+      if (p(h())) cons(h(),t().takeWhile(p))
+      else t().takeWhile(p)
+    case _ => empty
+  }
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+  //    this match {
+  //      case Cons(h, t) => p(h()) || t().exists(p)
+  //      case _ => false
+  //     }//works
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h,t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+  /*checks that all elements in the Stream match a given predicate.*/
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a,b) => p(a) && b)
+  /*this match {
+    case Cons(h,t) =>
+      if (p(h())) t() forAll p
+      else false
+    case _ => true
+  }*/
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((a,b) => cons(f(a), b))
+  def filter(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a,b) => if (f(a)) cons(a,b) else b)
+  def append[B>:A](s: => Stream[B]): Stream[B] =
+    foldRight(s)((a,b) => cons(a,b))
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((a,b) => f(a) append b)
+
+  def find(p: A => Boolean): Option[A] =
+    filter(p).headOption
 }
 case object Empty extends Stream[Nothing]
 /*The arguments we’d like to pass unevaluated have a () => immediately before their
@@ -63,11 +106,23 @@ object Stream extends App with Stream[String] {
 
   val stream2: Stream[Any] = cons({ () => 42}, Stream.empty)
   val stream1: Stream[Any] = cons(1, cons(2, Stream.empty) )
+  val stream5: Stream[String] = cons("1", cons("2", Stream.empty) )
   val stream3: Stream[Int] = cons(1, Stream.empty)
+  val stream4: Stream[Int] = cons(1,cons(2,cons(3,cons(4,cons(5,cons(6,cons(7, Stream.empty)))))))
 //  println(s"Elements of stream1 = ${stream1.headOption}")
 //  println(stream1.toList)
-  println(stream1.take(2).headOption)
-  println(stream1.drop(1).headOption)
-  println(stream3.take(2).headOption)
+  assert(stream1.take(2).headOption ==Some(1))
+  assert(stream1.drop(1).headOption == Some(2))
+  assert(stream3.take(1).headOption == Some(1))
+//  assert(stream4.takeWhile(_ % 2 == 0).toList == List(2,4,6))
+  println(stream4.takeWhile(_ % 2 == 0).toList)
+  assert(stream4.forAll(_ < 8) == true)
+  assert(stream4.forAll(_ != 7) == false)
+  assert(stream4.map(_ * 4).toList == List(4, 8, 12, 16, 20, 24, 28))
+  assert(stream4.filter(_ % 2 == 0).toList == List(2, 4, 6))
+  assert(stream4.append(stream4).toList == List(1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7))
+
+  val ones: Stream[Int] = Stream.cons(1, ones)
+  println(ones().toList)
 
 }
